2d68035b9c4416e8545dba8b9bd491818119ae32
Borsos Albert <borsosalbert@Borsos-MacBook-Air.local>
2014-07-30 11:19:12 +0200

    Mailer helper class added

---
+++ wrappers/Mailer.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borsosalbert
+ * Date: 2014.07.30.
+ * Time: 10:53
+ */
+
+namespace albertborsos\yii2lib\wrappers;
+
+
+use albertborsos\yii2lib\helpers\S;
+use yii\helpers\HtmlPurifier;
+use Yii;
+
+class Mailer {
+
+    public static function sendMail($to, $subject, $content, $from = null){
+
+        $from = self::setSender($from);
+
+        return Yii::$app->mailer->compose()
+            ->setSubject($subject)
+            ->setHtmlBody($content)
+            ->setTextBody(HtmlPurifier::process($content))
+            ->setFrom($from)
+            ->setReplyTo($from)
+            ->setTo($to)
+            ->send();
+    }
+
+    public static function sendMailByView($template, $params, $to, $subject, $from = null){
+        $content = Yii::$app->getView()->renderFile($template, $params);
+
+        return self::sendMail($to, $subject, $content, $from);
+    }
+
+    private static function setSender($from){
+        if (is_null($from)){
+            $from = S::get(Yii::$app->params, 'adminEmail');
+            if (is_null($from)){
+                $from = self::getDefaultSenderAddress();
+            }
+        }
+        return $from;
+    }
+
+    private static function getDefaultSenderAddress(){
+        $domain = Yii::$app->getUrlManager()->getHostInfo();
+        $needle = ['http://', 'https://', 'www'];
+        $domain = str_replace($needle, '', $domain);
+
+        return 'noreply@'.$domain;
+    }
+
+} 
