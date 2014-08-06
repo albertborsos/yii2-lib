226ca0f10cc67b8c78dcedffe61f087b7e05c5a1
Borsos Albert <borsosalbert@Borsos-MacBook-Air.local>
2014-08-06 14:33:06 +0200

    Date Helper class added

---
+++ helpers/Date.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borsosalbert
+ * Date: 2014.08.06.
+ * Time: 14:25
+ */
+
+namespace albertborsos\yii2lib\helpers;
+
+
+class Date {
+    public static function timestampToDate($timestamp, $formatTo = 'Y-m-d H:i'){
+        return date_format($timestamp, $formatTo);
+    }
+
+    public static function reformatDateTime($datetime, $formatFrom = 'Y-m-d H:i:s', $formatTo = 'Y-m-d H:i'){
+        return date_format(date_create_from_format($formatFrom, $datetime), $formatTo);
+    }
+} 
