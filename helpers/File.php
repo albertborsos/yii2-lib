1eecb6078a4c822f1772cc72103e4d9e6b888558
Borsos Albert <borsosalbert@Borsos-MacBook-Air.local>
2014-08-17 11:57:02 +0200

    File helper class added

---
+++ helpers/File.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borsosalbert
+ * Date: 2014.08.17.
+ * Time: 11:55
+ */
+
+namespace albertborsos\yii2lib\helpers;
+
+use yii\helpers\BaseFileHelper;
+
+class File extends BaseFileHelper {
+
+} 
