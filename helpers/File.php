309efe009d81a3cfa9a90366e1ea12b2712e0f8c
Borsos Albert <borsosalbert@Borsos-MacBook-Air.local>
2014-08-17 12:38:54 +0200

    dirContentToDataProvider() added

--- helpers/File.php
+++ helpers/File.php
@@ -8,8 +8,30 @@
 
 namespace albertborsos\yii2lib\helpers;
 
+use yii\data\ArrayDataProvider;
 use yii\helpers\BaseFileHelper;
 
 class File extends BaseFileHelper {
 
+    public static function dirContentToDataProvider($path, $options){
+        $source = [];
+        $dirContent = self::findFiles($path, $options);
+
+        foreach($dirContent as $id => $filePath){
+            $exploded = explode('/', $filePath);
+            $fileName = $exploded[count($exploded)-1];
+
+            $source[] = [
+                'id' => $id,
+                'fileName' => $fileName,
+                'filePath' => $filePath,
+            ];
+
+        }
+
+        return new ArrayDataProvider([
+            'allModels' => $source,
+            'key' => 'fileName',
+        ]);
+    }
 } 
