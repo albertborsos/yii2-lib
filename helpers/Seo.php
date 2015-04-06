d9bbcba549b5bc6a6e76c4c8bdb6cda4d2373bd1
Albert Borsos <albertborsos@me.com>
2015-04-06 21:07:30 +0200

    Seo class enhanced with FB specific tags

--- helpers/Seo.php
+++ helpers/Seo.php
@@ -19,36 +19,64 @@ class Seo {
 	const TYPE_TITLE = 'title';
 	const TYPE_ROBOTS = 'robots';
 
+    const TYPE_FB_TITLE = 'og:title';
+    const TYPE_FB_SITE_NAME = 'og:site_name';
+    const TYPE_FB_DESCRIPTION = 'og:description';
+    const TYPE_FB_IMAGE = 'og:image';
+    const TYPE_FB_TYPE = 'og:type';
+    const TYPE_FB_LOCALE = 'og:locale';
+    const TYPE_FB_SHARE_URL = 'og:url';
+    const TYPE_FB_APP_ID = 'og:app_id';
+
+    const FB_TYPE_PAGE = 'article';
+    const FB_TYPE_PRODUCT = 'product';
+    const FB_TYPE_PRODUCT_GROUP = 'product.group';
+
 	const INDEX = 'INDEX';
 	const NOINDEX = 'NOINDEX';
 
 	public static function registerTag($type, $content){
 		$view = Yii::$app->getView();
-		switch($type){
-			case self::TYPE_TITLE:
-				$appName = Yii::$app->name;
-				if(strpos($content, $appName)){
-					$view->title = $content;
-				}else{
-					$view->title = $content.' | '.$appName;
-				}
-				break;
-			case self::TYPE_CANONICAL:
-				$url = is_array($content) ? Yii::$app->getUrlManager()->createAbsoluteUrl($content) : $content;
-				$view->registerLinkTag([
-					'rel' => $type,
-					'href' => $url,
-				],$type);
-				break;
-			case self::TYPE_KEYWORDS:
-			case self::TYPE_DESCRIPTION:
-			case self::TYPE_ROBOTS:
-				$view->registerMetaTag([
-					'name' =>$type,
-					'content' => $content,
-				], $type);
-				break;
-		}
+        if(!is_null($content) && $content !== false){
+            switch($type){
+                case self::TYPE_TITLE:
+                    $appName = Yii::$app->name;
+                    if(strpos($content, $appName)){
+                        $view->title = $content;
+                    }else{
+                        $view->title = $content.' | '.$appName;
+                    }
+                    break;
+                case self::TYPE_CANONICAL:
+                        $url = is_array($content) ? Yii::$app->getUrlManager()->createAbsoluteUrl($content) : $content;
+                        $view->registerLinkTag([
+                            'rel' => $type,
+                            'href' => $url,
+                        ],$type);
+                    break;
+                case self::TYPE_KEYWORDS:
+                case self::TYPE_DESCRIPTION:
+                case self::TYPE_ROBOTS:
+                    $view->registerMetaTag([
+                        'name' =>$type,
+                        'content' => $content,
+                    ], $type);
+                    break;
+                case self::TYPE_FB_TITLE:
+                case self::TYPE_FB_SITE_NAME:
+                case self::TYPE_FB_TYPE:
+                case self::TYPE_FB_SHARE_URL:
+                case self::TYPE_FB_LOCALE:
+                case self::TYPE_FB_DESCRIPTION:
+                case self::TYPE_FB_IMAGE:
+                case self::TYPE_FB_APP_ID:
+                    $view->registerMetaTag([
+                        'property' => $type,
+                        'content' => $content,
+                    ]);
+                    break;
+            }
+        }
 	}
 
 	public static function noIndex(){
