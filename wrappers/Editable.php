57cbe12e6f995b74396d633c57b97bbfadbd128f
Albert Borsos <albertborsos@me.com>
2023-09-11 12:16:08 +0200

    fix to render selected options if source array has key-label pairs

--- wrappers/Editable.php
+++ wrappers/Editable.php
@@ -16,102 +16,114 @@ class Editable
 {
     public static function input($name, $pk, $defaultValue, $url, $pluginOptions = []): string
     {
-        return XEditable::widget(ArrayHelper::merge([
-            'name' => $name,
-            'value' => $defaultValue,
-            'url' => $url,
-            'type' => 'text',
-            'mode' => 'pop',
-            'clientOptions' => [
-                'pk' => $pk,
-                'placement' => 'top',
-            ]
-        ], $pluginOptions));
+        return XEditable::widget(
+            ArrayHelper::merge([
+                'name' => $name,
+                'value' => $defaultValue,
+                'url' => $url,
+                'type' => 'text',
+                'mode' => 'pop',
+                'clientOptions' => [
+                    'pk' => $pk,
+                    'placement' => 'top',
+                ]
+            ], $pluginOptions)
+        );
     }
 
     public static function textarea(string $defaultValue, array $url, array $widgetOptions = []): string
     {
-        return \kartik\editable\Editable::widget(ArrayHelper::merge([
-            'name' => 'value',
-            'asPopover' => true,
-            'displayValue' => $defaultValue,
-            'inputType' => \kartik\editable\Editable::INPUT_TEXTAREA,
-            'value' => $defaultValue,
-            'submitOnEnter' => false,
-            'size' => 'lg',
-            'formOptions' => [
-                'action' => $url,
-            ],
-            'options' => [
-                'class' => 'form-control',
-                'rows' => 5,
-            ],
-        ], $widgetOptions));
+        return \kartik\editable\Editable::widget(
+            ArrayHelper::merge([
+                'name' => 'value',
+                'asPopover' => true,
+                'displayValue' => $defaultValue,
+                'inputType' => \kartik\editable\Editable::INPUT_TEXTAREA,
+                'value' => $defaultValue,
+                'submitOnEnter' => false,
+                'size' => 'lg',
+                'formOptions' => [
+                    'action' => $url,
+                ],
+                'options' => [
+                    'class' => 'form-control',
+                    'rows' => 5,
+                ],
+            ], $widgetOptions)
+        );
     }
 
     public static function select($name, $pk, $defaultValue, $defaultText, $url, $sourceArray, $pluginOptions = []): string
     {
-        return XEditable::widget(ArrayHelper::merge([
-            'name' => $name,
-            'value' => $defaultText,
-            'url' => $url,
-            'type' => 'select',
-            'mode' => 'pop',
-            'clientOptions' => [
-                'pk' => $pk,
-                'defaultValue' => $defaultValue,
-                'placeholder' => $defaultText,
-                'placement' => 'top',
-                'source' => self::compileSourceArray($sourceArray)
-            ]
-        ], $pluginOptions));
+        return XEditable::widget(
+            ArrayHelper::merge([
+                'name' => $name,
+                'value' => $defaultText,
+                'url' => $url,
+                'type' => 'select',
+                'mode' => 'pop',
+                'clientOptions' => [
+                    'pk' => $pk,
+                    'defaultValue' => $defaultValue,
+                    'placeholder' => $defaultText,
+                    'placement' => 'top',
+                    'source' => self::compileSourceArray($sourceArray)
+                ]
+            ], $pluginOptions)
+        );
     }
 
     public static function select2($name, $pk, $defaultValue, $defaultText, $url, $sourceArray, $pluginOptions = []): string
     {
-        return XEditable::widget(ArrayHelper::merge([
-            'name' => $name,
-            'value' => $defaultValue,
-            'url' => $url,
-            'type' => 'select2',
-            'mode' => 'pop',
-            'clientOptions' => [
-                'pk' => $pk,
-                'defaultValue' => $defaultValue,
-                'placement' => 'top',
-                'select2' => [
-                    'width' => '230px'
-                ],
-                'source' => self::compileSourceArray($sourceArray)
-            ]
-        ], $pluginOptions));
+        return XEditable::widget(
+            ArrayHelper::merge([
+                'name' => $name,
+                'value' => $defaultValue,
+                'url' => $url,
+                'type' => 'select2',
+                'mode' => 'pop',
+                'clientOptions' => [
+                    'pk' => $pk,
+                    'defaultValue' => $defaultValue,
+                    'placement' => 'top',
+                    'select2' => [
+                        'width' => '230px'
+                    ],
+                    'source' => self::compileSourceArray($sourceArray)
+                ]
+            ], $pluginOptions)
+        );
     }
 
     public static function tags(string $defaultValue, array $url, array $sourceArray, array $widgetOptions = []): string
     {
         $editableId = uniqid('editable_');
-        return \albertborsos\yii2lib\widgets\Editable::widget(ArrayHelper::merge([
-            'name' => 'value',
-            'value' => $defaultValue,
-            'inputType' => \kartik\editable\Editable::INPUT_SELECT2,
-            'formOptions' => [
-                'action' => $url,
-            ],
-            'options' => [
-                'id' => $editableId,
-                'value' => explode(',', $defaultValue),
-                'initValueText' => $defaultValue,
-                'data' => array_combine(array_values($sourceArray), array_values($sourceArray)),
-                'showToggleAll' => false,
-                'pluginOptions' => [
-                    'dropdownParent' => "#{$editableId}-popover",
-                    'tags' => false,
-                    'multiple' => true,
-                    'allowClear' => true,
-                    'tokenSeparators' => ',',
+        return \albertborsos\yii2lib\widgets\Editable::widget(
+            ArrayHelper::merge([
+                'name' => 'value',
+                'value' => $defaultValue,
+                'inputType' => \kartik\editable\Editable::INPUT_SELECT2,
+                'formOptions' => [
+                    'action' => $url,
                 ],
-            ],
-        ], $widgetOptions));
+                'options' => [
+                    'id' => $editableId,
+                    'value' => self::get_keys_from_source_array($defaultValue, $sourceArray),
+                    'initValueText' => $defaultValue,
+                    'data' => ArrayHelper::isIndexed($sourceArray, true)
+                        ? array_combine(array_values($sourceArray), array_values($sourceArray))
+                        : $sourceArray,
+                    'showToggleAll' => false,
+                    'pluginOptions' => [
+                        'dropdownParent' => "#{$editableId}-popover",
+                        'tags' => false,
+                        'multiple' => true,
+                        'allowClear' => true,
+                        'tokenSeparators' => ',',
+                    ],
+                ],
+            ], $widgetOptions)
+        );
     }
 
     public static function compileSourceArray($sourceArray): array
@@ -126,6 +138,28 @@ class Editable
                 'value' => $key, // normal select
                 'text' => $value,
             ];
+        }
+        return $result;
+    }
+
+    /**
+     * Returns the keys of the source array based on the default values
+     * it is  necessary to preload the selected labels in the input field
+     *
+     * @param string $defaultValue
+     * @param array $sourceArray
+     * @return array
+     */
+    protected static function get_keys_from_source_array(string $defaultValue, array $sourceArray): array
+    {
+        if (ArrayHelper::isIndexed($sourceArray, true)) {
+            return explode(',', $defaultValue);
+        }
+
+        $defaultValues = explode(',', $defaultValue);
+        $result = [];
+        foreach ($defaultValues as $value) {
+            $result[] = array_search($value, $sourceArray);
         }
         return $result;
     }
